6c7baf576c54f8e75b84e38d517acccd715e31e1
seidenfuss <seidenfussbender@gmail.com>
2024-09-25 21:31:11 -0300

    add rent selector

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import plotly_express as px
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
 # Importar dados
-#df = pd.read_csv('houses_to_rent_v2.csv', decimal=",")
-df = pd.read_csv('ufma-dashboard/houses_to_rent_v2.csv', decimal=",")
+df = pd.read_csv('houses_to_rent_v2.csv', decimal=",")
+#df = pd.read_csv('ufma-dashboard/houses_to_rent_v2.csv', decimal=",")
 
 # Traduzir valores (pode ir para outro arquivo, modificar o documento e salvar como csv...)
 df['animal'] = df['animal'].replace('acept', 'Sim')
